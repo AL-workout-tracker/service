d6e9578d40c12afc987472b05cde9e4c2670eae9
AlecInfo <alecpiettepro@gmail.com>
2024-05-19 10:39:34 +0200

    add user controller

--- start/routes.ts
+++ start/routes.ts
@@ -8,37 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const UsersController = () => import('#controllers/UsersController')
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
-
-// routers pour les utilisateurs
-router.post('/users', 'UsersController.store')
-router.post('/login', 'UsersController.login')
-
-// routers pour les entraînements
-router
-  .group(() => {
-    router.get('/', 'WorkoutsController.index')
-    router.post('/', 'WorkoutsController.store')
-    router.get('/:id', 'WorkoutsController.show')
-    router.put('/:id', 'WorkoutsController.update')
-    router.delete('/:id', 'WorkoutsController.delete')
-  })
-  .prefix('/workouts')
-// .middleware(['auth'])
-
-// routers pour les exercices
-router
-  .group(() => {
-    router.get('/', 'ExercisesController.index')
-    router.post('/', 'ExercisesController.store')
-    router.get('/:id', 'ExercisesController.show')
-    router.put('/:id', 'ExercisesController.update')
-    router.delete('/:id', 'ExercisesController.delete')
-  })
-  .prefix('/exercises')
-  // .middleware(['auth_middleware'])
+router.post('/users', [UsersController, 'store'])
+router.post('/login', [UsersController, 'login'])
